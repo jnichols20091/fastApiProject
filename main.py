3b5d27d22e7314a69de7a3f2f2d35d854c1f9625
Josh Nichols <jnichols20091@gmail.com>
2023-02-01 02:45:24 -0600

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from fastapi.templating import Jinja2Templates
+import uvicorn
 
-app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app = FastAPI( )
 
 inventory = {
         1: {"name": "Foo",
@@ -20,6 +23,8 @@ async def say_hello(name: str):
 
 
 @app.get("/items/{item_id}")
-async def get_item(item_id: int):
+async def get_item(item_id: int = Path(None, description="The ID of the item you want to view", gt=0, lt=1000)):
     return inventory[item_id]
 
+if __name__ == "__main__":
+    uvicorn.run(app, port=8000)
